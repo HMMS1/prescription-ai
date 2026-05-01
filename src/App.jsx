91b06ebc77f4507514659c4c061109ca99452f7c
Hamza Mohammed Mostafa <167645878+HMMS1@users.noreply.github.com>
2026-05-01 19:16:18 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import ProtectedRoute from "./routes/ProtectedRoute";
 
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import LoginRegister from "./pages/LoginRegister";
 import UploadPrescription from "./pages/UploadPrescription";
@@ -16,43 +15,11 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/auth" element={<LoginRegister />} />
-
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/upload"
-          element={
-            <ProtectedRoute>
-              <UploadPrescription />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/pharmacies"
-          element={
-            <ProtectedRoute>
-              <Pharmacies />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/pharmacy-dashboard"
-          element={
-            <ProtectedRoute>
-              <PharmacyDashboard />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/upload" element={<UploadPrescription />} />
+        <Route path="/pharmacies" element={<Pharmacies />} />
+        <Route path="/pharmacy-dashboard" element={<PharmacyDashboard />} />
       </Routes>
     </>
   );
